c8ce22bd0110b00a7e8e42ee624c145e98ecbab7
sven <sven@so36.net>
2018-02-28 16:09:12 +0100

    meta: Add ttd-toctree

---
+++ ttd-toctree/dockerfiles/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#exec su-exec $@
+# Re-set permission to the `testuser` user
+# This avoids permission denied if the data volume is mounted by root
+chown -R testuser /build
+exec su-exec testuser /usr/local/bin/testtoctree.sh
---
+++ ttd-toctree/testtoctree.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Functions
+check_index(){
+	if [ ! -f index.rst ]; then
+    	echo -e "\e[1;31mCan not find a index.rst \e[0m"
+        exit 1
+	else
+		: # Do nothing and move on
+	fi
+}
+
+# First make sure that we have a index.rst
+check_index
+
+# Check for nested toctrees
+echo -e "\e[1mChecking for nested toctree entries \e[21m"
+ag --rest -F :numbered: --ignore index.rst
+if [ $? -eq 1 ]
+then
+    :
+    echo -e "\e[1;92mLooks great ! Awesome ! \e[0m"
+    exit 0
+else
+    echo -e "\e[1;31mFound one or more numbering entry, ':numbered:' is only allowed once in index.rst \e[0m"
+    exit 1
+fi
+exit 0
