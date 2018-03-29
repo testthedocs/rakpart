7aaeb539dd5508a5f433b3f1bef3b64094c2377a
sven <sven@so36.net>
2018-03-29 13:37:28 +0200

    fix: #1

--- ttd-ts/dockerfiles/entrypoint.sh
+++ ttd-ts/dockerfiles/entrypoint.sh
@@ -19,4 +19,4 @@ fi
 # Re-set permission to the `ttd` user
 # This avoids permission denied if the data volume is mounted by root
 chown -R ttd /build
-exec su-exec ttd /usr/local/bin/check.sh
+exec su-exec ttd /usr/local/bin/testts.sh
