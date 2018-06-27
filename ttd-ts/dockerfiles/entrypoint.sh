04935e5963c1c383297a54f3fe728c2fca19bd5d
sven <sven@so36.net>
2018-06-27 16:05:28 +0200

    code: Unify with the rest of our tests

--- ttd-ts/dockerfiles/entrypoint.sh
+++ ttd-ts/dockerfiles/entrypoint.sh
@@ -28,5 +28,5 @@ fi
 # Re-set permission to the `ttd` user
 # This avoids permission denied if the data volume is mounted by root
 chown -R ttd /build
-exec su-exec ttd /usr/local/bin/testts.sh
+exec su-exec ttd /usr/local/bin/check.sh
 
