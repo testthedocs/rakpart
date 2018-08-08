5796e550282e7cc258ae8b24f400c120aac27125
sven <sven@so36.net>
2018-08-08 16:48:01 +0200

    docker: Move to Multi-stage build

--- ttd-vale/dockerfiles/entrypoint.sh
+++ ttd-vale/dockerfiles/entrypoint.sh
@@ -4,6 +4,6 @@ set -e
 #exec su-exec $@
 # Re-set permission to the `ttd` user
 # This avoids permission denied if the data volume is mounted by root
-chown -R ttd /src
-exec su-exec ttd vale --config /src/.vale.ini "$@"
+chown -R ttd /srv
+exec su-exec ttd vale  "$@"
 
