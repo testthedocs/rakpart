319608272f669d1613264f1a41092e61c6a418f4
sven <sven@so36.net>
2018-08-09 12:30:57 +0200

    docker: Improve container

--- ttd-vale/dockerfiles/entrypoint.sh
+++ ttd-vale/dockerfiles/entrypoint.sh
@@ -5,5 +5,5 @@ set -e
 # Re-set permission to the `ttd` user
 # This avoids permission denied if the data volume is mounted by root
 chown -R ttd /srv
-exec su-exec ttd vale  "$@"
-
+#exec su-exec ttd vale "$@"
+exec su-exec ttd vale /srv/tests
