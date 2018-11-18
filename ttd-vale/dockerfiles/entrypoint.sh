d3a195d7dbde767fdefbda963275a321b9aadba7
Chris Ward <chriswhward@gmail.com>
2018-11-18 15:13:51 +0100

    Allow specifying of files to check

--- ttd-vale/dockerfiles/entrypoint.sh
+++ ttd-vale/dockerfiles/entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 set -e
 
+# DEBUG
+echo '$0 = ' $0
+echo '$1 = ' $1
+
 #exec su-exec $@
 # Re-set permission to the `ttd` user
 # This avoids permission denied if the data volume is mounted by root
 chown -R ttd /srv
 #exec su-exec ttd vale "$@"
-exec su-exec ttd vale /srv/tests
+exec su-exec ttd vale /srv/tests/"$1"
