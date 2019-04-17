6746930f9bc745b95ddc750d69f7f883def28cc5
sven <sven@so36.net>
2019-04-17 14:32:07 +0200

    [meta] WIP new entrypoint script

--- ttd-vale/dockerfiles/entrypoint.sh
+++ ttd-vale/dockerfiles/entrypoint.sh
@@ -3,6 +3,6 @@ set -eo pipefail
 
 # Re-set permission to the `ttd` user
 # This avoids permission denied if the data volume is mounted by root
-# chown -R ttd /srv
+chown -R ttd /srv
 #exec su-exec ttd vale "$@"
-exec vale /srv/tests
+exec su-exec ttd vale /srv/tests
